002679c8b07dc0a9e7ce467eb4779fbfccd8a015
ayamalekcher <ayamalek.cherchar@univ-constantine2.dz>
2025-11-23 17:36:37 +0100

    Initial commit

---
+++ src/test/java/com/example/demoTest/DemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.demoTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
